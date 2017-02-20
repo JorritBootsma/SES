89a607c8109799c78e1974493af6438b675540e0
Jorrit Bootsma <jorritbootsma@gmail.com>
2017-02-20 11:20:28 +0100

    Create file

---
+++ SES-ass1.py
@@ -0,0 +1,11 @@
+######################################################################
+#   Structure and Evolution of Stars - SES-ass1.py
+#   20/02/2017
+#   Jorrit Bootsma
+#	10251499
+######################################################################
+
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as spInt
